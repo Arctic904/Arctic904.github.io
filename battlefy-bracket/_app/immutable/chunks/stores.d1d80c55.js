526193780d48a33a1cd464525efc6bffe1db3a0d
Ryan <arcticfire904@gmail.com>
2023-09-02 15:07:11 -0400

    Add battlefy

---
+++ battlefy-bracket/_app/immutable/chunks/stores.d1d80c55.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.1b5e448f.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
